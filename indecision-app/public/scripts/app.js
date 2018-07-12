a4f12e646edd5dd378db7a13a8c0f05320f38b0c
Amy Kirasack <amykirasack@gmail.com>
2018-07-12 16:39:15 -0400

    Explore nested elements with JSX and do first challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,54 @@ console.log('App.js is running');
 
 //JSX JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX from app.js! Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Here\'s some info'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'thing one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'thing two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Amy'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 27'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Ottawa'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is running')
 
 //JSX JavaScript XML
-let template = <h1>This is JSX from app.js! Does this change?</h1>;
-let appRoot = document.getElementById('app');
+const template = (
+    <div>
+        <h1>Indecision App!</h1> 
+        <p>Here's some info</p>
+        <ul>
+            <li>thing one</li>
+            <li>thing two</li>
+        </ul>
+    </div>
+);
 
-ReactDOM.render(template, appRoot);
+const templateTwo = (
+    <div>
+        <h1>Amy</h1>
+        <p>Age: 27</p>
+        <p>Location: Ottawa</p>
+    </div>
+)
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
