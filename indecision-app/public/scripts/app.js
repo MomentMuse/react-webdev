d8a479a2fb0e0feb93aeb6350da8e5236e3b4b0a
Amy Kirasack <amykirasack@gmail.com>
2018-07-12 16:07:39 -0400

    Install babel, add comment to app.js, yarn init

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,6 +3,7 @@ console.log('App.js is running')
 //JSX JavaScript XML
 // let template = <p>This is JSX from app.js!</p>;
 
+//this code will be compiled
 let template = React.createElement(
     "h1",
     { id: 'someid' },
