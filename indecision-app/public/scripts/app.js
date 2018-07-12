c08512189ccc986f3d1cbf8efe145dd86609880d
Amy Kirasack <amykirasack@gmail.com>
2018-07-12 15:50:45 -0400

    Add some React code Babelified, add another div to index.html

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,14 @@
 console.log('App.js is running')
+
+//JSX JavaScript XML
+// let template = <p>This is JSX from app.js!</p>;
+
+let template = React.createElement(
+    "h1",
+    { id: 'someid' },
+    'Something new',
+);
+
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
