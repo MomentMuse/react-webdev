bd00d73d5b609b0f0c557ba3afd6c5bcfb9bbc69
Amy Kirasack <amykirasack@gmail.com>
2018-07-14 23:46:04 -0400

    Compare ES5 regular functions and ES6 arrow functions

---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,10 @@
+const square = function (x) {
+    return x * x;
+}
+
+console.log(square(8))
+
+//arrow functions are always anonymous, so you need to assign it to a variable
+const squareArrow = (x) => x * x ;
+
+console.log(squareArrow(8));
