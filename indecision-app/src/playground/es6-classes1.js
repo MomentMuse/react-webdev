7025d7885e683030fa7e9c78810868d406a0d3fe
Amy Kirasack <amykirasack@gmail.com>
2018-07-23 21:28:11 -0400

    Do constructor challenge

--- indecision-app/src/playground/es6-classes1.js
+++ indecision-app/src/playground/es6-classes1.js
@@ -1,14 +1,20 @@
 class Person {
-    constructor(name = 'Anonymous') {
+    constructor(name = 'Anonymous', age = 0) {
         this.name = name;
+        this.age = age;
     }
     getGreeting() {
         return `Hi ${this.name}!`;
     }
+    getDescription() {
+        return `${this.name} is ${this.age} years old.`
+    }
 }
 
-const me = new Person('Amy Kirasack');
+const me = new Person('Amy Kirasack', 27);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());
