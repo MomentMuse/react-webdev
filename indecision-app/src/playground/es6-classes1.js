3c3f2aa5c70288d877619e9b82a418185232cca8
Amy Kirasack <amykirasack@gmail.com>
2018-07-23 21:24:52 -0400

    Add another method to class

--- indecision-app/src/playground/es6-classes1.js
+++ indecision-app/src/playground/es6-classes1.js
@@ -2,10 +2,13 @@ class Person {
     constructor(name = 'Anonymous') {
         this.name = name;
     }
+    getGreeting() {
+        return `Hi ${this.name}!`;
+    }
 }
 
 const me = new Person('Amy Kirasack');
-console.log(me);
+console.log(me.getGreeting());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
