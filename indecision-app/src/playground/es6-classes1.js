39ecb74a38503d5e0e9f6bf000f50432b6b3d9d3
Amy Kirasack <amykirasack@gmail.com>
2018-07-23 21:07:11 -0400

    Set up constructor function

--- indecision-app/src/playground/es6-classes1.js
+++ indecision-app/src/playground/es6-classes1.js
@@ -1,6 +1,11 @@
 class Person {
-
+    constructor(name = 'Anonymous') {
+        this.name = name;
+    }
 }
 
-const me = new Person();
+const me = new Person('Amy Kirasack');
 console.log(me);
+
+const other = new Person();
+console.log(other);
